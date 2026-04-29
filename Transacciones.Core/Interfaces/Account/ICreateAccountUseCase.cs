6f1557460d80f90c40b3cb27028d82702a63e417
Andres Sebastian Guerrero Camacho <andres1druchito2@gmail.com>
2026-04-29 19:09:38 -0400

    add create account use case

---
+++ Transacciones.Core/Interfaces/Account/ICreateAccountUseCase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Transacciones.Core.Models.Account.CreateAccount;
+
+namespace Transacciones.Core.Interfaces.Account
+{
+    public interface ICreateAccountUseCase
+    {
+        Task<CreateAccountResponse> ExecuteAsync(CreateAccountRequest request, CancellationToken cancellationToken = default);
+    }
+}
---
+++ Transacciones.Core/UseCases/Account/CreateAccountUseCase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Transacciones.Core.Models.Account.CreateAccount;
+
+namespace Transacciones.Core.UseCases.Account
+{
+    public class CreateAccountUseCase
+    {
+        public Task<CreateAccountResponse> ExecuteAsync(CreateAccountRequest request, CancellationToken cancellationToken = default)
+        {
+            Task.CompletedTask.Wait();
+            return Task.FromResult(new CreateAccountResponse
+            {
+                Id = request.Id,
+                AccountNumber = request.AccountNumber,
+                Balance = request.Balance,
+                Holder = request.Holder,
+                CreatedAt = request.CreatedAt,
+                IsActive = request.IsActive
+            });
+        }
+
+    }
+}
